149a29d675c2b5b878034a79e003494934cdeea5
Ben Rodriguez <76826504+BenRodriguez1029@users.noreply.github.com>
2025-09-30 23:08:01 -0400

    Update losses.py

--- losses.py
+++ losses.py
@@ -5,23 +5,22 @@ from keras import ops
 
 
 def cross_entropy_loss(y_true, y_pred):
-    return ops.mean(-y_true * ops.log(y_pred + 1e-7) - (1.0 - y_true) * ops.log(1.0 - y_pred + 1e-7), axis=-1)
+    return -y_true * ops.log(y_pred + 1e-7) - (1.0 - y_true) * ops.log(1.0 - y_pred + 1e-7)
 
 def squared_loss(y_true, y_pred):
-    return ops.mean(ops.square(y_pred - y_true), axis=-1)
+    return ops.square(y_pred - y_true)
 
 def polynomial_loss(y_true, y_pred, alpha=2.0):
-    return ops.mean(y_true * ops.power(1.0 - y_pred, alpha) +
-                    (1.0 - y_true) * ops.power(y_pred, alpha), axis=-1)
+    return y_true * ops.power(1.0 - y_pred, alpha) + (1.0 - y_true) * ops.power(y_pred, alpha)
 
 def exponential_loss(y_true, y_pred):
-    return ops.mean(ops.exp((0.5 - y_true) * y_pred), axis=-1)
+    return ops.exp((0.5 - y_true) * y_pred)
 
 def logistic_loss(y_true, y_pred):
-    return ops.mean(ops.log(1.0 + ops.exp((1.0 - 2.0 * y_true) * y_pred)), axis=-1)
+    return ops.log(1.0 + ops.exp((1.0 - 2.0 * y_true) * y_pred))
 
 def alpha_exponential_loss(y_true, y_pred, alpha=2.0):
-    return ops.mean(ops.power(1.0 + ops.exp((1.0 - 2.0 * y_true) * y_pred), alpha), axis=-1)
+    return ops.power(1.0 + ops.exp((1.0 - 2.0 * y_true) * y_pred), alpha)
 
 def alpha_log_exponential_loss(y_true, y_pred, alpha=2.0):
     return ops.power(y_pred, (0.5 - y_true) * alpha)
@@ -31,4 +30,6 @@ def J_alpha(x, alpha=2.0):
     return x + x * ops.power(ops.abs(x), alpha - 1)
 
 def lpop_exponential_loss(y_true, y_pred, alpha=2.0):
-    return ops.mean(ops.exp((0.5 - y_true) * J_alpha(y_pred, alpha)), axis=-1)
+    return ops.exp((0.5 - y_true) * J_alpha(y_pred, alpha))
+
+
