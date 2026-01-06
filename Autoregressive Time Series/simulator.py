9250604360b77a11f9456299fd2fb176a7f29cee
Ben Rodriguez <76826504+BenRodriguez1029@users.noreply.github.com>
2026-01-05 19:01:10 -0500

    Fixed ar sim

--- Autoregressive Time Series/simulator.py
+++ Autoregressive Time Series/simulator.py
@@ -1,23 +1,31 @@
 from math import sqrt
 import random
+import numpy as np
 
-def epsilon():
-    return random.gauss(0, 1)
 
 def autoregress(r, rho, v):
-    return (rho * r) + sqrt(1-rho**2) * v * epsilon() 
+    return (rho * r) + sqrt(1-rho**2) * v * random.gauss(0, 1)
 
-def simulate_ar_series(r_0, rho_dist, v_dist, n, fixed=False):
-    if fixed:
-        rho = rho_dist()
-        v = v_dist()
 
-    series = [[r_0]]
-    for t in range(1, n):
-        if not fixed:
-            rho = rho_dist()
-            v = v_dist()
-        r_t = autoregress(series[t-1][0], rho, v)
-        series.append([r_t])
+def random_walk(n_steps):
+    r_t = 0
+    rho_t = random.uniform(-1, 1)
+    v_t = random.uniform(0, 0.006)
 
-    return series
+    returns = []
+    models = []
+
+    for t in range(n_steps):
+        if random.random() < 0.01:
+            rho_t = random.uniform(-1, 1)
+            v_t = random.uniform(0, 0.006)
+            models.append(1)
+        else:
+            rho_t = np.clip(rho_t + random.gauss(0, 0.05), -1, 1)
+            v_t = np.clip(v_t + random.gauss(0, 0.0001), 0, 0.006)
+            models.append(0)
+
+        r_t = autoregress(r_t, rho_t, v_t)
+        returns.append(r_t)
+
+    return returns, models
