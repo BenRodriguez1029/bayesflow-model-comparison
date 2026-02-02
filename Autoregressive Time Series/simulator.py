18b5749d185d5c06eb451637f149b7884e76d480
Benjamin Rodriguez <ben.24.rodriguez@gmail.com>
2026-02-02 16:55:34 -0500

    used np for randomness

--- Autoregressive Time Series/simulator.py
+++ Autoregressive Time Series/simulator.py
@@ -1,28 +1,27 @@
 from math import sqrt
-import random
 import numpy as np
 
 
 def autoregress(r, rho, v):
-    return (rho * r) + sqrt(1-rho**2) * v * random.gauss(0, 1)
+    return (rho * r) + sqrt(1-rho**2) * v * np.random.standard_normal()
 
 
 def random_walk(n_steps):
     r_t = 0
-    rho_t = random.uniform(-1, 1)
-    v_t = random.uniform(0, 0.006)
+    rho_t = np.random.uniform(-1, 1)
+    v_t = np.random.uniform(0, 0.006)
 
     returns = []
     models = []
 
     for t in range(n_steps):
-        if random.random() < 0.3:
-            rho_t = random.uniform(-1, 1)
-            v_t = random.uniform(0, 0.006)
+        if np.random.random() < 0.3:
+            rho_t = np.random.uniform(-1, 1)
+            v_t = np.random.uniform(0, 0.006)
             models.append(1)
         else:
-            rho_t = np.clip(rho_t + random.gauss(0, 0.05), -1, 1)
-            v_t = np.clip(v_t + random.gauss(0, 0.0001), 0, 0.006)
+            rho_t = np.clip(rho_t + np.random.normal(0, 0.05), -1, 1)
+            v_t = np.clip(v_t + np.random.normal(0, 0.0001), 0, 0.006)
             models.append(0)
 
         r_t = autoregress(r_t, rho_t, v_t)
